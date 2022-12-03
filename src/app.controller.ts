9cbe3abea8495b3b93c0c8d841de19996fe859cb
Nadeera Sampath <nadeera@Nadeera.local>
2022-12-03 21:08:19 +0530

    log service added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AxiomService } from './axiom.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly axiomService : AxiomService
+   ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/users')
+  async getUsers(@Res() response){
+    await this.axiomService.log('error', "Success Message");
+    return response.status(HttpStatus.OK).json({
+      type: 'success',
+      message: 'Users has been fetched successfully',
+      data: null,
+    });
+  }
+
 }
